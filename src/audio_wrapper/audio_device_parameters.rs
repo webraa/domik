a52550f75e59136167316fec5079f36bf1e2e9a0
AlexanderARodin <rodin_aa@inbox.ru>
2024-01-04 15:51:42 +0300

    saving

---
+++ src/audio_wrapper/audio_device_parameters.rs
@@ -0,0 +1,37 @@
+use tinyaudio::prelude::OutputDeviceParameters;
+
+use crate::raadbg::log;
+
+pub struct AudioDeviceParameters {
+    pub sample_rate: usize,
+    pub block_size: usize,
+    pub blocks_count: usize,
+}
+
+impl AudioDeviceParameters {
+    pub fn new() -> Self {
+        Self {
+            sample_rate: 44100,
+            block_size: 441,
+            blocks_count: 8
+        }
+    }
+    pub fn get_output_device_parameters(&self) -> OutputDeviceParameters {
+        OutputDeviceParameters{
+            sample_rate: self.sample_rate,
+            channels_count: 2,
+            channel_sample_count: self.block_size * self.blocks_count
+        }
+    }
+    pub fn get_tick_time(&self) -> f32 {
+        let res = 2. * (self.block_size as f32) / (self.sample_rate as f32);
+        log::simple( format!("tick time = {res}").as_str() );
+        return res;
+    }
+}
+
+impl Default for AudioDeviceParameters {
+    fn default() -> Self {
+        Self::new()
+    }
+}
