9d2c576d4a34af4847ccf9a50be3907be9dd760b
AlexanderARodin <rodin_aa@inbox.ru>
2024-01-10 21:35:13 +0300

    saving

--- assets/sw.js
+++ assets/sw.js
@@ -8,8 +8,10 @@ var filesToCache = [
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
+  console.log('--> raa 3')
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
+      console.log('--> raa 3.1')
       return cache.addAll(filesToCache);
     })
   );
@@ -18,9 +20,12 @@ self.addEventListener('install', function (e) {
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function (e) {
+  console.log('--> raa 4')
   e.respondWith(
     caches.match(e.request).then(function (response) {
+      console.log('--> raa 4.1')
       return response || fetch(e.request);
     })
   );
 });
+
